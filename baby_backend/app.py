8b46e66aa57b14b55e006234a2a08ff996e38be4
travinne <travinnetgm1@gmail.com>
2025-11-12 20:37:03 +0300

    new new push changed the app.py on port

--- baby_backend/app.py
+++ baby_backend/app.py
@@ -78,4 +78,6 @@ def home():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True, use_reloader=False)
